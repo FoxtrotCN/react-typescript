b92355f749bf5f49f176e90916888b3664330c2a
Fernando <fernandostarlin1012@gmail.com>
2023-04-29 11:09:54 -0400

    Using the children Prop to pass children to a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import "./App.css";
 import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  const items = ["New York", "San Francisco", "Silicon Valley", "Sturgeon Bay"];
-
-  const handleSelectedItem = (item: string) => {
-    console.log(item);
-  };
+  // const items = ["New York", "San Francisco", "Silicon Valley", "Sturgeon Bay"];
+  //
+  // const handleSelectedItem = (item: string) => {
+  //   console.log(item);
+  // };
   return (
     <>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectedItem={handleSelectedItem}
-      />
+      {/*<ListGroup*/}
+      {/*  items={items}*/}
+      {/*  heading="Cities"*/}
+      {/*  onSelectedItem={handleSelectedItem}*/}
+      {/*/>*/}
+      <Alert>This is a children Alert</Alert>
     </>
   );
 }
